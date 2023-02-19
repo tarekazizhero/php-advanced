263774beea845c787f8f8e171177420550b155b1
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-20 03:46:53 +0600

    get method

---
+++ 1.getMethod/test.php
@@ -0,0 +1,12 @@
+<form action="home.php" method="GET" >
+
+<input type="text" name="username" placeholder="username" > <br>
+<input type="password" name="password" placeholder="password"> <br>
+<input type="submit" value="submit"> 
+
+
+
+
+
+
+</form>
