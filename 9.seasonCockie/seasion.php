2ce456e23e25c88a4234a8c5802d144f825dd194
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-21 03:22:37 +0600

    session  process

---
+++ 9.seasonCockie/seasion.php
@@ -0,0 +1,9 @@
+<?php
+
+    session_start();
+    $_SESSION['user']="aziz";
+
+    echo $_SESSION['user'];
+    //session a protybar echo korle ,,session_start() ati korte hy
+    //ja cookie te korte hy na
+?>
